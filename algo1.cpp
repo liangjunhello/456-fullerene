d088374bb4cd5a866b5245e78033c7c427cf4076
liangjunhello <44309832+liangjunhello@users.noreply.github.com>
2022-08-30 22:02:51 +0800

    Update algo1.cpp

--- algo1.cpp
+++ algo1.cpp
@@ -5,7 +5,7 @@
 #include "algo3.cpp"
 #include "algo4.cpp"
 using namespace std;
-
+// An algorithm determining the cyclic vertex connectivity of  (4, 5, 6)-fullerene graphs.
 int algo1(const vector<vector<int>> &e, int g, int m)
 { // using parameter m to avoid calculate m again, same as algo.2
     int n = e.size() - 1;
