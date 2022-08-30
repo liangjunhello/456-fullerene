d2f7a728c6eb23edae298058975f9f010ec41fcb
liangjunhello <44309832+liangjunhello@users.noreply.github.com>
2022-08-30 22:03:40 +0800

    Update algo2.cpp

--- algo2.cpp
+++ algo2.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "algo4.cpp"
 using namespace std;
+// An algorithm determining the cyclic edge connectivity of  (4, 5, 6)-fullerene graphs.
 int algo2(const vector<vector<int>> &e, int g, int m)
 {
     if (g == 5)
